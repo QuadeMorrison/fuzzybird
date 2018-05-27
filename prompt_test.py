70ba5cc1e127a759d6adfd10bb8f22e030a8a325
Alan Morgan <alanxoc3@gmail.com>
2018-05-27 00:01:30 -0600

    good fzf test now :)

--- prompt_test.py
+++ prompt_test.py
@@ -2,17 +2,34 @@
 
 import curses
 import string
+import shutil
 from curses import wrapper
+import signal, sys
+
+g_lines, g_cols = 0, 0
 
 START_CURSOR_X=2
 
-tweet_arr = ["asoueth", "bob", "bill", "quade", "colton", "alan"]
+tweet_arr = ["aeou", "bob", "bill", "quade", "colton", "alan"]
 cursor_x, cursor_y = 0, 0
 
 spot = 0
 
-def draw_scr(stdscr, input_str):
+input_string = ""
+finish_winch = False
+
+char_codes = []
+for c in string.printable:
+    char_codes.append(ord(c))
+
+def draw_scr(stdscr):
+    global input_string
+
+    stdscr.move(0, 0)
     stdscr.clear()
+
+    cursor_y = curses.LINES-1
+    stdscr.move(cursor_y, cursor_x)
 
     for i, tw in enumerate(tweet_arr):
         if i < curses.LINES-1:
@@ -23,24 +40,18 @@ def draw_scr(stdscr, input_str):
     if spot >= 0:
         stdscr.addnstr(curses.LINES-2-spot, 0, "> ", START_CURSOR_X)
 
-    stdscr.addstr(curses.LINES-1, 0, "> " + input_str)
+    stdscr.addnstr(curses.LINES-1, 0, "> " + input_string, curses.COLS-1)
     stdscr.refresh()
 
 def main(stdscr):
-    input_string = ""
     cursor_x = START_CURSOR_X
     cursor_y = curses.LINES-1
-    global spot
+    global spot, input_string, finish_winch, g_lines, g_cols
 
-    def draw():
-        draw_scr(stdscr, input_string)
-
-    draw()
+    draw_scr(stdscr)
 
     while True:
-        stdscr.move(cursor_y, cursor_x)
         ch_code = stdscr.getch()
-        ch = chr(ch_code)
         if ch_code == curses.KEY_UP:
             spot = min(len(tweet_arr)-1, spot + 1)
         elif ch_code == curses.KEY_DOWN:
@@ -51,9 +62,18 @@ def main(stdscr):
         elif ch_code == 10:
             # enter key
             break
-        elif ch in string.printable and cursor_x < curses.COLS-1:
+        elif ch_code in char_codes and cursor_x < curses.COLS-1:
             cursor_x += 1
-            input_string += ch
+            input_string += chr(ch_code)
+        elif ch_code == curses.KEY_RESIZE:
+            # Assume WINCH
+            input_string = ""
+            cursor_x = START_CURSOR_X
+            spot = 0
+            l, c = stdscr.getmaxyx()
+            if g_lines != l or g_cols != c:
+                curses.resizeterm(l, c)
+                g_lines, g_cols = l, c
 
         # MUST UPDATE THE SPOT HERE!
         # THEN DRAW!!!
@@ -63,7 +83,7 @@ def main(stdscr):
         else:
             spot = min(tl, max(0, spot))
 
-        draw()
+        draw_scr(stdscr)
 
 wrapper(main)
 
